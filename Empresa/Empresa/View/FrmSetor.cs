0872d831667f9a1fe9f2558c7f3910a3e8fd2ed5
SamuelCarlos <50051459+Sam-CS@users.noreply.github.com>
2020-01-30 22:15:00 -0300

    Add files via upload

---
+++ Empresa/Empresa/View/FrmSetor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Empresa.View
+{
+    public partial class FrmSetor : Form
+    {
+        Setor setor;
+        Conexao con;
+        public FrmSetor()
+        {
+            InitializeComponent();
+            setor = new Setor();
+            con = new Conexao();
+        }
+
+        public void lerDados()
+        {
+            Setor objSetor = new Setor();
+            objSetor.id = int.Parse(txtId.Text.ToString());
+            objSetor.nome = txtNome.Text;
+            if (rbAtivado.Checked)
+            {
+                objSetor.status = 1;
+            }
+            else
+            {
+                objSetor.status = 0;
+            }
+
+        }
+
+        public void atualizarGrid()
+        {
+            List<Setor> listSetor = new List<Setor>();
+            con.OpenConnection();
+            MySqlDataReader reader;
+            reader = con.executeQuery("SELECT * FROM tb_setor");
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Setor objSetor = new Setor();
+                    objSetor.id = reader.GetInt32(0);
+                    objSetor.nome = reader.GetString(1);
+                    objSetor.status = reader.GetValue(3).ToString() == "True" ? 1 : 0;
+
+                    listSetor.Add(objSetor);
+                
+                }
+                reader.Close();
+            }
+            else
+            {
+                Console.WriteLine("Não retornou dados!");
+            }
+
+            
+
+        }
+
+        
+       
+    }
+}
--- Empresa/Empresa/View/FrmUsuario.Designer.cs
+++ Empresa/Empresa/View/FrmUsuario.Designer.cs
@@ -28,11 +28,276 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GroupBox = new System.Windows.Forms.GroupBox();
+            this.cmbPerfil = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbAtivo = new System.Windows.Forms.RadioButton();
+            this.rbDesativo = new System.Windows.Forms.RadioButton();
+            this.dgvUsuario = new System.Windows.Forms.DataGridView();
+            this.GroupBox.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GroupBox
+            // 
+            this.GroupBox.Controls.Add(this.groupBox2);
+            this.GroupBox.Controls.Add(this.cmbPerfil);
+            this.GroupBox.Controls.Add(this.label7);
+            this.GroupBox.Controls.Add(this.label5);
+            this.GroupBox.Controls.Add(this.txtUsuario);
+            this.GroupBox.Controls.Add(this.txtSenha);
+            this.GroupBox.Controls.Add(this.label4);
+            this.GroupBox.Controls.Add(this.txtEmail);
+            this.GroupBox.Controls.Add(this.label3);
+            this.GroupBox.Controls.Add(this.txtNome);
+            this.GroupBox.Controls.Add(this.label2);
+            this.GroupBox.Controls.Add(this.label1);
+            this.GroupBox.Controls.Add(this.txtId);
+            this.GroupBox.Location = new System.Drawing.Point(12, 87);
+            this.GroupBox.Name = "GroupBox";
+            this.GroupBox.Size = new System.Drawing.Size(664, 153);
+            this.GroupBox.TabIndex = 0;
+            this.GroupBox.TabStop = false;
+            this.GroupBox.Text = "Dados";
+            // 
+            // cmbPerfil
+            // 
+            this.cmbPerfil.FormattingEnabled = true;
+            this.cmbPerfil.Location = new System.Drawing.Point(411, 91);
+            this.cmbPerfil.Name = "cmbPerfil";
+            this.cmbPerfil.Size = new System.Drawing.Size(121, 21);
+            this.cmbPerfil.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(415, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Perfil";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(198, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Usuário";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(201, 92);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(188, 20);
+            this.txtUsuario.TabIndex = 8;
+            this.txtUsuario.UseWaitCursor = true;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(15, 92);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(153, 20);
+            this.txtSenha.TabIndex = 7;
+            this.txtSenha.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Senha";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(411, 41);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(247, 20);
+            this.txtEmail.TabIndex = 5;
+            this.txtEmail.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(408, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(149, 41);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(240, 20);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "lD";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(15, 41);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(41, 12);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(86, 29);
+            this.btnNovo.TabIndex = 1;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(213, 12);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(86, 29);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(371, 12);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(86, 29);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(527, 12);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(86, 29);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbDesativo);
+            this.groupBox2.Controls.Add(this.rbAtivo);
+            this.groupBox2.Location = new System.Drawing.Point(557, 76);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(101, 64);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // rbAtivo
+            // 
+            this.rbAtivo.AutoSize = true;
+            this.rbAtivo.Location = new System.Drawing.Point(7, 18);
+            this.rbAtivo.Name = "rbAtivo";
+            this.rbAtivo.Size = new System.Drawing.Size(49, 17);
+            this.rbAtivo.TabIndex = 0;
+            this.rbAtivo.TabStop = true;
+            this.rbAtivo.Text = "Ativo";
+            this.rbAtivo.UseVisualStyleBackColor = true;
+            // 
+            // rbDesativo
+            // 
+            this.rbDesativo.AutoSize = true;
+            this.rbDesativo.Location = new System.Drawing.Point(7, 41);
+            this.rbDesativo.Name = "rbDesativo";
+            this.rbDesativo.Size = new System.Drawing.Size(67, 17);
+            this.rbDesativo.TabIndex = 1;
+            this.rbDesativo.TabStop = true;
+            this.rbDesativo.Text = "Desativo";
+            this.rbDesativo.UseVisualStyleBackColor = true;
+            // 
+            // dgvUsuario
+            // 
+            this.dgvUsuario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuario.Location = new System.Drawing.Point(12, 259);
+            this.dgvUsuario.Name = "dgvUsuario";
+            this.dgvUsuario.Size = new System.Drawing.Size(664, 196);
+            this.dgvUsuario.TabIndex = 5;
+            // 
+            // FrmUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmUsuario";
+            this.ClientSize = new System.Drawing.Size(688, 467);
+            this.Controls.Add(this.dgvUsuario);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.GroupBox);
+            this.Name = "FrmUsuario";
+            this.Text = "Usuario";
+            this.GroupBox.ResumeLayout(false);
+            this.GroupBox.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox GroupBox;
+        private System.Windows.Forms.ComboBox cmbPerfil;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbDesativo;
+        private System.Windows.Forms.RadioButton rbAtivo;
+        private System.Windows.Forms.DataGridView dgvUsuario;
     }
 }
